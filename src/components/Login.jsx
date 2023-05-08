b05153d55bf59d6dbc28e4b403e7606e5c995473
Trương Văn Đạt <datvan635@gmail.com>
2023-05-08 23:14:28 +0700

    Done

---
+++ src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import { useNavigate } from 'react-router-dom';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const reponseGoogle = (response) => {
+    const details = jwt_decode(response.credential);
+    // console.log(details);
+
+    localStorage.setItem('user', JSON.stringify(details));
+
+    const { name, sub, picture } = details;
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    }
+
+    console.log(doc);
+
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate('/', { replace: true })
+      })
+  }
+
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="flex justify-start items-center flex-col h-screen">
+        <div className="relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} alt="logg" width="130px" />
+            </div>
+            <div className="shadow-2xl">
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+                buttonText="Login with Google"
+                onSuccess={reponseGoogle}
+                onFailure={(err) => console.log(err)}
+                cookiePolicy={"single_host_origin"}
+                responseType="code,token"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default Login
